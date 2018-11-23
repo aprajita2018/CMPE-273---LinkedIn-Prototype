312fb5e88bb0ac753175b901fc2f04c49f9ed0c4
aprajita2018 <aprajita212@gmail.com>
2018-11-22 17:22:54 -0800

    NavBar alignments and moving to Bootstrap4

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -70,58 +70,61 @@ class NavBar extends Component {
         return(
             <div>
                 <nav className ="navbar navbar-expand-sm" id="mainNav" >
-                    <div className="container d-flex">
-                        {/* Landing Page Navbar */}
-                        <div className="navbar-header align-middle" style={{display: this.state.isLoggedIn? 'none': 'flex'}}>
-                            <a className='navbar-brand'><img alt="LinkedIn" src="img/navbarLogo.png" height="25px"/></a>
-                        </div>
-
-                        <form className="navbar-form navbar-right form-inline" style={{display: this.state.isLoggedIn? 'none': 'flex'}} onSubmit={this.onSubmit}>
-                            <div className='input-group input-group-sm'>
-                                <input type='email'    className='form-control' name='email'    placeholder='Email' value={this.state.email} onChange={this.onChange}/>
+                    <div className="container">
+                        <div className="d-flex w-100">
+                            {/* Landing Page Navbar */}
+                            <div className='navbar-brand' style={{display: this.state.isLoggedIn? 'none': 'flex'}}>
+                                <img alt="LinkedIn" src="img/navbarLogo.png" className="align-self-center" height="30px"/>
                             </div>
-                            <div className='input-group input-group-sm mx-2'>
-                                <input type='password' className='form-control input-sm' name='password' placeholder='Password' value={this.state.password} onChange={this.onChange} />
-                            </div>  
-                            <button type='submit' className='btn btn-outline-light btn-sm'>Sign In</button>
-                        </form>
-                        
-                        {/* Logged In Navbar */}
-                        <div className="navbar-header" style={{display: this.state.isLoggedIn? 'flex': 'none'}}>
-                            <a className='navbar-brand'><img alt="LinkedIn" src="img/loggedinLogo.png" height="30px"/></a>
-                        </div>
-                        <div className='d-flex justify-content-center' style={{display: this.state.isLoggedIn? 'flex': 'none'}}>
-                            <ul className='navbar-nav ml-auto' style={{display: this.state.isLoggedIn? 'flex': 'none'}}>
-                                <li className='nav-item mx-4'>
-                                    <Link to="/"><span className='icon-text'><i className="fas fa-user-friends fa-2x"></i><br/>My Network</span></Link>                                 
-                                </li>
-                                <li className='nav-item mx-4'>
-                                    <Link to="/"><span className='icon-text'><i className="fas fa-briefcase fa-2x"></i><br/>Jobs</span></Link>                                 
-                                </li>
-                                <li className='nav-item mx-4'>
-                                    <Link to="/"><span className='icon-text'><i className="fas fa-envelope fa-2x"></i><br/>Messaging</span></Link>                                 
-                                </li>
-                                <li className="nav-item mx-4 dropdown">
-                                    <a className="dropdown-toggle text-white" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">
-                                        <img src='img/JaneDoe.png' alt='Profile Pic' height='30' width='30' />
-                                        <br/><span className='icon-text '>Me</span> 
-                                    </a>
-                                    <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                                        <div className='dropdown-item' >
-                                            <span><img src='img/JaneDoe.png' alt='profile pic' height='60' width='60' /></span>
-                                            <div className='navbar-profile-name'>{this.props.name}</div>
-                                            <div className='navbar-profile-title'>Title</div>
-                                            <br/>
-                                            <Link to="/profile"><span className='text-info'>View Profile</span></Link>                                 
 
+                            <form className="navbar-form navbar-right ml-auto form-inline" style={{display: this.state.isLoggedIn? 'none': 'flex'}} onSubmit={this.onSubmit}>
+                                <div className='input-group input-group-sm'>
+                                    <input type='email'    className='form-control' name='email'    placeholder='Email' value={this.state.email} onChange={this.onChange}/>
+                                </div>
+                                <div className='input-group input-group-sm mx-2'>
+                                    <input type='password' className='form-control input-sm' name='password' placeholder='Password' value={this.state.password} onChange={this.onChange} />
+                                </div>  
+                                <button type='submit' className='btn btn-outline-light btn-sm'>Sign In</button>
+                            </form>
+                            
+                            {/* Logged In Navbar */}
+                            <div className='navbar-brand' style={{display: this.state.isLoggedIn? 'flex': 'none'}}>
+                                <img alt="LinkedIn" src="img/loggedinLogo.png" className="align-self-center" height="35px"/>
+                            </div>
+
+                            <div className='align-self-center align-items-end ml-auto' style={{display: this.state.isLoggedIn? 'flex': 'none'}}>
+                                <ul className='navbar-nav' style={{display: this.state.isLoggedIn? 'flex': 'none'}}>
+                                    <li className='nav-item nav-link mx-4 py-0'>
+                                        <Link to="/"><span className='icon-text'><i className="fas fa-user-friends fa-2x"></i><br/>My Network</span></Link>                                 
+                                    </li>
+                                    <li className='nav-item nav-link mx-4 py-0'>
+                                        <Link to="/"><span className='icon-text'><i className="fas fa-briefcase fa-2x"></i><br/>Jobs</span></Link>                                 
+                                    </li>
+                                    <li className='nav-item nav-link mx-4 py-0'>
+                                        <Link to="/"><span className='icon-text'><i className="fas fa-envelope fa-2x"></i><br/>Messaging</span></Link>                                 
+                                    </li>
+                                    <li className="nav-item nav-link mx-4 py-0 dropdown">
+                                        <a className="dropdown-toggle text-white" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">
+                                            <img src='img/JaneDoe.png' alt='Profile Pic' height='30' width='30' />
+                                            <br/><span className='icon-text '>Me</span> 
+                                        </a>
+                                        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                                            <div className='dropdown-item' >
+                                                <span><img src='img/JaneDoe.png' alt='profile pic' height='60' width='60' /></span>
+                                                <div className='navbar-profile-name'>{this.props.name}</div>
+                                                <div className='navbar-profile-title'>Title</div>
+                                                <br/>
+                                                <Link to="/profile"><span className='text-info'>View Profile</span></Link>                                 
+
+                                            </div>
+                                            <span className='dropdown-item text-secondary'><button className='btn btn-danger' onClick={this.handlelogout}>Sign Out</button></span>                            
                                         </div>
-                                        <span className='dropdown-item text-secondary'><button className='btn btn-danger' onClick={this.handlelogout}>Sign Out</button></span>                            
-                                    </div>
-                                </li>
-                                <li className='nav-item  mx-4' style={{display: this.props.user_type === 'recruiter'? 'flex': 'none'}}>
-                                    <Link to="/jobpost"><span className='icon-text'><i className="fas fa-file-alt fa-2x"></i><br/>Post Jobs</span></Link>                                 
-                                </li>
-                            </ul>
+                                    </li>
+                                    <li className='nav-item nav-link py-0 mx-4' style={{display: this.props.user_type === 'recruiter'? 'flex': 'none'}}>
+                                        <Link to="/jobpost"><span className='icon-text'><i className="fas fa-file-alt fa-2x"></i><br/>Post Jobs</span></Link>                                 
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>                    
                 </nav>
